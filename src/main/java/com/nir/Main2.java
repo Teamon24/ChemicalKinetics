60badb54e68dfb7baa897d5b1bb5a5773263059d
Artem Leshchenko <Teamon24@github.com>
2020-03-28 14:35:23 +0400

    a few edits

--- src/main/java/com/nir/Main2.java
+++ src/main/java/com/nir/Main2.java
@@ -7,10 +7,13 @@ import com.nir.ui.beans.ReactionParser;
 import com.nir.ui.utils.CSS;
 import com.nir.ui.utils.Reaction;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.stage.Stage;
 
@@ -24,6 +27,8 @@ public class Main2 extends Application {
     private Stage addReactionStage;
     private Stage chooseReactionStage;
 
+    public static final EventHandler<ActionEvent> EXIT = ignored -> System.exit(0);
+
     public void start(Stage primaryStage) {
         chooseReactionStage = primaryStage;
         try {
@@ -33,32 +38,7 @@ public class Main2 extends Application {
             primaryStage.show();
 
             Button btnAddNew = (Button) chooseReactionScene.lookup("#btnAddNew");
-            btnAddNew.setOnMouseClicked(mouseEvent -> {
-                try {
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/add_reaction.fxml"));
-                    addReactionScene = new Scene(fxmlLoader.load());
-                    addReactionStage = new Stage();
-                    addReactionStage.setScene(addReactionScene);
-                    addReactionStage.show();
-                    Button btnSave = (Button) addReactionScene.lookup("#btnSave");
-                    btnSave.setOnMouseClicked(mouseEvent1 -> {
-                        //update reactions list in chooseReactionScene
-                        addReactionStage.close();
-
-                    });
-                    Button btnCancel = (Button) addReactionScene.lookup("#btnCancel");
-                    btnCancel.setOnMouseClicked(mouseEvent1 -> {
-                        addReactionStage.close();
-
-                    });
-                    addReactionStage.setOnHiding(windowEvent -> {
-                        chooseReactionStage.show();
-                    });
-                    chooseReactionStage.hide();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
+            btnAddNew.setOnMouseClicked(mouseEvent -> showAddReactionForm());
             Button btnOK = (Button) chooseReactionScene.lookup("#btnOK");
             btnOK.setOnMouseClicked(mouseEvent -> {
                 ArrayList<Reaction> reactions = new ArrayList<>();
@@ -66,38 +46,60 @@ public class Main2 extends Application {
                 String reactionString = (String) comboBox.getValue();
                 //connect to database
                 //search reaction in db, get id of complex reaction, find reactions with foreign key
-                ReactionParser parser = ReactionParser.INSTANCE;
                 //for...{
-                    RawReaction rawReaction = parser.parse(reactionString);
-                    Reaction reaction = parser.convert(rawReaction);
+                    RawReaction rawReaction = ReactionParser.parse(reactionString);
+                    Reaction reaction = ReactionParser.convert(rawReaction);
                     reactions.add(reaction);
                 // }
                 // составляем стехиометрическую матрицу, систему, решаем, получаем результат, открываем мейн форму, отображаем результат
-                try {
-                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main_form.fxml"));
-                    mainScene = new Scene(fxmlLoader.load());
-                    mainStage = new Stage();
-                    mainStage.setScene(mainScene);
-                    mainStage.show();
-                    chooseReactionStage.hide();
-                    mainStage.setOnHiding(windowEvent -> {
-                        chooseReactionStage.show();
-                    });
-                    MenuBar menuBar = (MenuBar) mainScene.lookup("#menuBar");
-                    menuBar.getMenus().get(0).getItems().get(0).setOnAction(actionEvent -> {
-                        mainStage.close();
-                        chooseReactionStage.show();
-                    });
-                    menuBar.getMenus().get(0).getItems().get(1).setOnAction(actionEvent -> {
-                        System.exit(0);
-                    });
-                    menuBar.getMenus().get(0).getItems().get(2).setOnAction(actionEvent -> {
-                        System.exit(0);
-                    });
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                showMainForm();
             });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void showMainForm() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/main_form.fxml"));
+            mainScene = new Scene(fxmlLoader.load());
+            mainStage = new Stage();
+            mainStage.setScene(mainScene);
+            mainStage.show();
+            chooseReactionStage.hide();
+            mainStage.setOnHiding(windowEvent -> chooseReactionStage.show());
+
+            MenuBar menuBar = (MenuBar) mainScene.lookup("#menuBar");
+            final Menu submenu = menuBar.getMenus().get(0);
+            submenu.getItems().get(0).setOnAction(actionEvent -> {
+                mainStage.close();
+                chooseReactionStage.show();
+            });
+            submenu.getItems().get(1).setOnAction(EXIT);
+            submenu.getItems().get(2).setOnAction(EXIT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void showAddReactionForm() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/add_reaction.fxml"));
+            addReactionScene = new Scene(fxmlLoader.load());
+            addReactionStage = new Stage();
+            addReactionStage.setScene(addReactionScene);
+            addReactionStage.show();
+            Button btnSave = (Button) addReactionScene.lookup("#btnSave");
+            btnSave.setOnMouseClicked(mouseEvent1 -> {
+                //update reactions list in chooseReactionScene
+                addReactionStage.close();
+            });
+            Button btnCancel = (Button) addReactionScene.lookup("#btnCancel");
+            btnCancel.setOnMouseClicked(mouseEvent1 -> addReactionStage.close());
+            addReactionStage.setOnHiding(windowEvent -> chooseReactionStage.show());
+            chooseReactionStage.hide();
         } catch (Exception e) {
             e.printStackTrace();
         }
